36d1e2546fa94c028131094d14566fdba8e5d317
Unknown <zster1204@gmail.com>
2018-11-26 12:35:14 +0100

    Mark / Zoran folder added

---
+++ Assets/Dev/Scripts/Zoran/Controls.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controls : MonoBehaviour
+{
+	void Update ()
+    {
+        if (Input.GetMouseButton(0))
+        {
+
+        }
+    }
+}
---
+++ Assets/Dev/Scripts/Zoran/Trash.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trash : MonoBehaviour
+{
+    public GameObject trash;
+
+    public void Update()
+    {
+        
+    }
+}
