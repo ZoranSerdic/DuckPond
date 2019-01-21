cfe806e2204c6a2664e12451bd91991a2de731bb
Unknown <zster1204@gmail.com>
2019-01-21 11:25:01 +0100

    Timer script added / Timer canvas added

---
+++ Assets/Dev/Scripts/Zoran/Timer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    public TextMeshProUGUI text;
+
+    public float realTime = 0.0f;
+    public float Time;
+
+    void Update()
+    {
+        realTime += UnityEngine.Time.deltaTime;
+        Time = Mathf.Floor(realTime);
+
+        text.text = "" + Time;
+    }
+}
